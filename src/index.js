89f8beff8a0704b916df7ed37c666f5bd6c59f59
anjibabu <bandianjibabu8@gmail.com>
2018-10-09 14:57:13 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './Css/tabs.css';
 import './Css/tabstyles.css';
-import Signup from  './Signup';
+
 // import 'font-awesome/css/font-awesome.min.css';
 // import 'bootstrap-css-only/css/bootstrap.min.css'; 
 // import 'mdbreact/dist/css/mdb.css';
@@ -11,6 +11,6 @@ import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Signup/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 serviceWorker.unregister();
