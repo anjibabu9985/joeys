79f6ba35d77ebf37fe5cb857d359de46ded91ad3
anjibabu <bandianjibabu8@gmail.com>
2018-10-09 17:45:24 +0530

    Updates

---
+++ precache-manifest.c38ef33417fbce87e24f310baa725da9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf7fb6a274f63207e3030de1c37242c",
+    "url": "/xyz/static/media/pic1.cdf7fb6a.png"
+  },
+  {
+    "revision": "dfe0201aa03ffee4af2d",
+    "url": "/xyz/static/css/main.e9070c79.chunk.css"
+  },
+  {
+    "revision": "05cefa48ba6109da6a66",
+    "url": "/xyz/static/js/1.05cefa48.chunk.js"
+  },
+  {
+    "revision": "4d948eff3493ffae2f99",
+    "url": "/xyz/static/js/runtime~main.4d948eff.js"
+  },
+  {
+    "revision": "c70582ace59a21310b39b838b19ef5e6",
+    "url": "/xyz/static/media/room.c70582ac.jpg"
+  },
+  {
+    "revision": "f96223f80a36cc259cb9cfa2ef968ad5",
+    "url": "/xyz/static/media/logo.f96223f8.png"
+  },
+  {
+    "revision": "dfe0201aa03ffee4af2d",
+    "url": "/xyz/static/js/main.dfe0201a.chunk.js"
+  },
+  {
+    "revision": "f3d808ba495da023eabc6bfe57dd3ebd",
+    "url": "/xyz/static/media/pic2.f3d808ba.png"
+  },
+  {
+    "revision": "43e3813465f1c70038034fdc5c9acd2d",
+    "url": "/xyz/static/media/pic3.43e38134.png"
+  },
+  {
+    "revision": "12a7941255ed60772b540f1b89d38389",
+    "url": "/xyz/static/media/home2.12a79412.jpg"
+  },
+  {
+    "revision": "16ec5dc00c8c099c9281afce043358ea",
+    "url": "/xyz/static/media/bg.16ec5dc0.jpg"
+  },
+  {
+    "revision": "caf47f41eed91d04d243b94f2692bc56",
+    "url": "/xyz/index.html"
+  }
+];
