0c12a384c63aac2f12805cff34b24b52c2b1cb98
anjibabu <bandianjibabu8@gmail.com>
2018-10-09 15:18:25 +0530

    Updates

---
+++ precache-manifest.67ba20b933394ce50e8bedeb61df8410.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf7fb6a274f63207e3030de1c37242c",
+    "url": "/joeys/static/media/pic1.cdf7fb6a.png"
+  },
+  {
+    "revision": "6b7ab5b0a149fdf1cfc9",
+    "url": "/joeys/static/css/main.e9070c79.chunk.css"
+  },
+  {
+    "revision": "05cefa48ba6109da6a66",
+    "url": "/joeys/static/js/1.05cefa48.chunk.js"
+  },
+  {
+    "revision": "db9828a4fa78f471aeca",
+    "url": "/joeys/static/js/runtime~main.db9828a4.js"
+  },
+  {
+    "revision": "c70582ace59a21310b39b838b19ef5e6",
+    "url": "/joeys/static/media/room.c70582ac.jpg"
+  },
+  {
+    "revision": "f96223f80a36cc259cb9cfa2ef968ad5",
+    "url": "/joeys/static/media/logo.f96223f8.png"
+  },
+  {
+    "revision": "6b7ab5b0a149fdf1cfc9",
+    "url": "/joeys/static/js/main.6b7ab5b0.chunk.js"
+  },
+  {
+    "revision": "f3d808ba495da023eabc6bfe57dd3ebd",
+    "url": "/joeys/static/media/pic2.f3d808ba.png"
+  },
+  {
+    "revision": "43e3813465f1c70038034fdc5c9acd2d",
+    "url": "/joeys/static/media/pic3.43e38134.png"
+  },
+  {
+    "revision": "12a7941255ed60772b540f1b89d38389",
+    "url": "/joeys/static/media/home2.12a79412.jpg"
+  },
+  {
+    "revision": "16ec5dc00c8c099c9281afce043358ea",
+    "url": "/joeys/static/media/bg.16ec5dc0.jpg"
+  },
+  {
+    "revision": "e91f5f1619e849e28b08ac63730eabb9",
+    "url": "/joeys/index.html"
+  }
+];
